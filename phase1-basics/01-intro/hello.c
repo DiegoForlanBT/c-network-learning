9d1dbfd89492c513e6ae6cf65c33da949dc559a7
DiegoForlanBT <sirDiegoForlan@gmail.com>
2026-06-30 23:20:59 +0300

    mod01: hello world - ilk C programi

---
+++ phase1-basics/01-intro/hello.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+
+int main(){
+
+
+    //printf("Hello World\n");
+    //printf("Network için c programlama !!!");
+
+    printf("Merhaba Dünya\n");
+    printf("Adım : Barış\n");
+    printf("Hedef : C ne network\n");
+    printf("Dil : C");
+
+
+    return 0 ;
+}
